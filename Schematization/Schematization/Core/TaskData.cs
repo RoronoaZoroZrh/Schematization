8f7b3e089fa0212c9e8b164056df75ab9a605924
RoronoaZoroZrh <137147783@qq.com>
2018-02-27 14:19:39 +0800

    添加任务描述

---
+++ Schematization/Schematization/Core/TaskData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Schematization
+{
+    public class TaskData
+    {
+        //!任务状态
+        public TaskState State
+        {
+            get
+            {
+                return m_vTaskState;
+            }
+            set
+            {
+                m_vTaskState = value;
+            }
+        }
+        private TaskState m_vTaskState = TaskState.UnFinish;
+
+        //!任务名称
+        public String Name
+        {
+            get
+            {
+                return m_sTaskName;
+            }
+            set
+            {
+                m_sTaskName = value;
+            }
+        }
+        private String m_sTaskName = "";
+
+        //!任务描述
+        public String Desc
+        {
+            get
+            {
+                return m_sTaskDesc;
+            }
+            set
+            {
+                m_sTaskDesc = value;
+            }
+        }
+        private String m_sTaskDesc = "";
+    }
+}
